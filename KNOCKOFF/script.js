3b66501638710eaf9eaf2400d1ab9d00177e4b39
Oliver Ren <128006873+RoboticOllie@users.noreply.github.com>
2023-04-24 12:42:03 +1200

    Update script.js

--- KNOCKOFF/script.js
+++ KNOCKOFF/script.js
@@ -63,15 +63,45 @@ function animate(){
             continue
         }
     }
-    p1.update(ctx, dt, platform)
-    p2.update(ctx, dt, platform)
+    p1.update(ctx, dt, platform, p2)
+    p2.update(ctx, dt, platform, p1)
+    // if the players collide, make them both be hit back by calling hit(), and then make it so they can go on top of each other later
+    if(p1.x + p1.width > p2.x && p1.x < p2.x + p2.width && p1.y + p1.height > p2.y && p1.y < p2.y + p2.height){
+        p1.hit(-p1.velX, 0, 100)
+        p2.hit(-p2.velX, 0, 100)
+    }
+    if(p1.y > platform.y + platform.height){
+        p2.score += 1
+        reset()
+    }
+    if(p2.y > platform.y + platform.height){
+        p1.score += 1
+        reset()
+    }
+
+    p1HealthBar = document.getElementById('p1Health')
+    p2HealthBar = document.getElementById('p2Health')
+    p1ScoreCounter = document.getElementById('p1Score')
+    p2ScoreCounter = document.getElementById('p2Score')
+    p1HealthBar.style.width = p1.maxHealth / 100 * p1.health + '%'
+    p2HealthBar.style.width = p2.maxHealth / 100 * p2.health + '%'
+    p1ScoreCounter.innerHTML = p1.score
+    p2ScoreCounter.innerHTML = p2.score
+    if(p1.health <= 0){
+        p1.score += 1
+        reset()
+    }
+    if(p2.health <= 0){
+        p2.score += 1
+        reset()
+    }
     requestAnimationFrame(animate)
 }
 animate()
 
 addEventListener("keydown", (e) => {
     // Player 1
-    if(p1.fallingBack == 0){
+    if(p1.fallingBack == false){
         if(e.key.toLowerCase() == "a"){
             p1.move(-1)
         }
@@ -83,7 +113,7 @@ addEventListener("keydown", (e) => {
         p1.aiming = true
     }
     // Player 2
-    if(p2.fallingBack == 0){
+    if(p2.fallingBack == false){
         if(e.key == "ArrowLeft"){
             p2.move(-1)
         }
@@ -100,6 +130,9 @@ addEventListener("keyup", (e) => {
     if(e.key.toLowerCase() == "a" || e.key.toLowerCase() == "d"){
         p1.move(0)
     }
+    if(e.key.toLowerCase() == "w" && p1.fallingBack == false){
+        p1.jump(platform, p2)
+    }
     if(e.key.toLowerCase() == "f"){
         p1.aiming = false
         p1.fire(p1.aimAngle)
@@ -109,9 +142,17 @@ addEventListener("keyup", (e) => {
     if(e.key == "ArrowLeft" || e.key == "ArrowRight"){
         p2.move(0)
     }
+    if(e.key == "ArrowUp" && p2.fallingBack == false){
+        p2.jump(platform, p1)
+    }
     if(e.key == " "){
         p2.aiming = false
         p2.fire(p2.aimAngle)
         p2.aimAngle = 0
     }
 })
+
+function reset(){
+    p1.reset()
+    p2.reset()
+}
